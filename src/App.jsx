ca5b7bd757090830753543b59b24cc3ae1d782c2
Solana Dev10 <solanadeveloper10@gmail.com>
2025-01-31 12:56:55 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import Contract from './Contract'
+
+import './App.css'
+
+function App() {
+  return <>
+    <div className='color-bg' />
+    <div className='wrapper'>
+      <header className='header'>
+        <a href="#" target='_blank' className='social-link'>
+          X
+        </a>
+        <a href="#" target='_blank' className='social-link'>
+          Telegram
+        </a>
+        <a href="#" target='_blank' className='social-link'>
+          Dextools
+        </a>
+        <a href="#" target='_blank' className='social-link'>
+          Dexscreener
+        </a>
+        <a href="#" target='_blank' className='social-link'>
+          Raydium
+        </a>
+      </header>
+
+      <div className='image-wrapper'>
+        <img src="/hoodfart.png" alt="Fork" className='image' />
+      </div>
+
+      <Contract />
+    </div>
+
+  </>
+}
+
+export default App
